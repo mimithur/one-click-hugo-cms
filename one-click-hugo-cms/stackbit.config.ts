ef6925cc3d13022d84b50a6bd41adae16dbe6453
mimithur <rthurber@silvercommpanama.com>
2024-12-27 12:24:27 -0500

    Update stackbit.config.ts

--- one-click-hugo-cms/stackbit.config.ts
+++ one-click-hugo-cms/stackbit.config.ts
@@ -3,7 +3,7 @@ import { defineStackbitConfig } from '@stackbit/types';
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
     "nodeVersion": "18",
-    "ssgName": "custom",
+    "ssgName": "nextjs",
     "contentSources": [],
     "postInstallCommand": "npm i --no-save @stackbit/types"
 })
